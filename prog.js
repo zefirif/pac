f8a95db49b8d540fcb459779396bc884e1b1156f
Your Name <you@example.com>
2023-11-14 20:15:46 +0100

    enfin

---
+++ prog.js
@@ -0,0 +1,13 @@
+let cercle = document.querySelector('.cercle');
+let cj = document.getElementById('id1');
+function myFunction() {
+    var x = document.createElement("BUTTON");
+    cercle.style.background = 'black' ;
+    cj.style.background = 'black' ;
+}
+
+// var elem = document.getElementById("id1");
+// let interval = setInterval(() => {
+//     console.log("admin");
+// })
+// console.log(elem)
