774f3d8cc27f9b4a5a475c592d28d0ecf17e882d
Your Name <you@example.com>
2023-11-14 20:19:13 +0100

    end_14/11/2023

--- prog.js
+++ prog.js
@@ -1,9 +1,11 @@
 let cercle = document.querySelector('.cercle');
 let cj = document.getElementById('id1');
+let cv = document.getElementById('gre');
 function myFunction() {
     var x = document.createElement("BUTTON");
-    cercle.style.background = 'black' ;
-    cj.style.background = 'black' ;
+    cercle.style.background = 'darkslategray' ;
+    cj.style.background = 'darkslategray' ;
+    cv.style.background = 'darkslategray' ;
 }
 
 // var elem = document.getElementById("id1");
